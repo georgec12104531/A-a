2cef5d7906b59a23c441cbb95d661de22a3b60ca
George Charles Chang <31757304+georgec12104531@users.noreply.github.com>
2018-09-03 18:28:07 -0700

    Add files via upload

---
+++ W3D1/skeleton/lib/03a_nobel_bonus.rb
@@ -0,0 +1,34 @@
+# == Schema Information
+#
+# Table name: nobels
+#
+#  yr          :integer
+#  subject     :string
+#  winner      :string
+
+require_relative './sqlzoo.rb'
+
+# BONUS PROBLEM: requires sub-queries or joins. Attempt this after completing
+# sections 04 and 07.
+
+def physics_no_chemistry
+  # In which years was the Physics prize awarded, but no Chemistry prize?
+  execute(<<-SQL)
+  SELECT 
+    yr
+
+  FROM 
+    (SELECT *
+    FROM 
+      nobels 
+    WHERE 
+      subject != 'Chemistry' GROUP BY subject) AS none_chem_yrs
+      
+  WHERE 
+    subject = 'Physics'
+  SQL
+end
+
+# how would selfjoins work for this would we select the years where physics in a winner
+# and then 
+# 
