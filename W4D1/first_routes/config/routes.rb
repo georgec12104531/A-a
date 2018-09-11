7895ddda037e1e8b6320c024ce2dd7b1a68b7290
George Charles Chang <31757304+georgec12104531@users.noreply.github.com>
2018-09-10 19:36:06 -0700

    Add files via upload

---
+++ W4D1/first_routes/config/routes.rb
@@ -0,0 +1,20 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # resources :users
+  
+  # def get(:users)
+  #   User.all
+  # end
+  
+  
+  get 'users', to: 'users#index', as: 'users'
+  post 'users', to: 'users#create', as: 'create_users'
+  get 'users/new', to: 'users#new', as: 'new_user'
+  get 'users/edit/:id', to: 'users#edit', as: 'edit_user'
+  get 'users/:id', to: 'users#show', as: 'user'
+  patch 'users/:id', to: 'users#update'
+  put 'users/:id', to: 'users#update'
+  delete 'users/:id', to: 'users#destroy', as: 'delete_user'
+  
+  
+end
---
+++ W4D1/first_routes/db/migrate/20180910235107_create_artworks_table.rb
@@ -0,0 +1,15 @@
+class CreateArtworksTable < ActiveRecord::Migration[5.2]
+  def change
+    create_table :artworks do |t|
+      t.string :title, presence: true
+      t.string :image_url, presence: true
+      t.string :artist_id, presence: true
+      
+      t.timestamps
+    end
+    
+    add_index :artworks, [:title, :artist_id], unique: true
+    add_index :artworks, :artist_id
+    
+  end
+end
